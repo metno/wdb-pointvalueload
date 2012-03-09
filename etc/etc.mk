09cc086a42eb2c70b0409be0900fc49c1b86039c
Aleksandar Babic <aleksandar.babic@met.no>
2012-03-09 16:40:34 +0100

    fixing compilation

--- etc/etc.mk
+++ etc/etc.mk
@@ -3,14 +3,12 @@ pkgsysconf_DATA = 	etc/loadfelt.cfg \
                         etc/loadgrib.cfg \
                         etc/loadgribeps.cfg \
                         etc/test.nc \
-
                         etc/felt/fimexreader.xml \
                         etc/felt/dataprovider.conf \
 		        etc/felt/validtime.conf \
 			etc/felt/valueparameter.conf \
 			etc/felt/levelparameter.conf \
 			etc/felt/leveladditions.conf \
-
                         etc/felteps/felt2nc_vars_eps.xml \
                         etc/felteps/felt_axes.xml \
                         etc/felteps/felt_global_attributes.xml \
@@ -20,7 +18,6 @@ pkgsysconf_DATA = 	etc/loadfelt.cfg \
 			etc/felteps/valueparameter.conf \
 			etc/felteps/levelparameter.conf \
 			etc/felteps/leveladditions.conf \
-
                         etc/grib/fimexreader.xml \
                         etc/grib/dataprovider.conf \
 			etc/grib/valueparameter1.conf \
--- src/src.mk
+++ src/src.mk
@@ -3,19 +3,19 @@
 #-----------------------------------------------------------------------------
 
 pointLoad_SOURCES =  src/main.cpp \
-                     src/CfgFileReader.cpp \               
+                     src/CfgFileReader.cpp \
                      src/CmdLine.cpp \
-                     src/DBConnection.cpp \  
-                     src/FeltLoader.cpp \ 
-                     src/GribField.cpp \  
-                     src/GribFile.cpp \ 
+                     src/DBConnection.cpp \
+                     src/FeltLoader.cpp \
+                     src/GribField.cpp \
+                     src/GribFile.cpp \
                      src/GribGridDefinition.cpp \
-                     src/GribHandleReader.cpp \ 
+                     src/GribHandleReader.cpp \
                      src/GribHandleReaderInterface.cpp \
                      src/GribLoader.cpp \
                      src/Loader.cpp \
                      src/WriteFloat.hpp \
-                     src/CfgFileReader.h \  
+                     src/CfgFileReader.h \
                      src/CmdLine.h \
                      src/DBConnection.hpp \
                      src/FeltLoader.h \
@@ -25,7 +25,7 @@ pointLoad_SOURCES =  src/main.cpp \
                      src/GribHandleReader.hpp \
                      src/GribHandleReaderInterface.hpp \
                      src/GribLoader.hpp \
-                     src/Loader.hpp 
+                     src/Loader.hpp  \
                      src/WciSession.hpp
 	
 EXTRA_DIST +=		src/src.mk
