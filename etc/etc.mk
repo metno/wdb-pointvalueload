df0611c07aba8047abfb6b8bd2ce7367473be1dd
Aleksandar Babic <aleksandarb@wdb-dev3.met.no>
2012-03-16 08:25:36 +0000

    installing some example config files

--- etc/etc.mk
+++ etc/etc.mk
@@ -1,24 +1,24 @@
-dist_etcpointload_DATA += 	etc/template.nc
+dist_etcpointload_DATA += etc/template.nc \
+                          etc/loadfelteps.cfg \
+                          etc/felteps/felt2nc_vars_eps.xml \
+                          etc/felteps/felt_axes.xml \
+                          etc/felteps/felt_global_attributes.xml \
+                          etc/felteps/felt_variables_eps.xml \
+                          etc/felteps/dataprovider.conf \
+                          etc/felteps/validtime.conf \
+                          etc/felteps/valueparameter.conf \
+                          etc/felteps/levelparameter.conf \
+                          etc/felteps/leveladditions.conf
+
                         #etc/loadfelt.cfg \
-                        #etc/loadfelteps.cfg \
                         #etc/loadgrib.cfg \
                         #etc/loadgribeps.cfg \
-                        #etc/test.nc \
                         #etc/felt/fimexreader.xml \
                         #etc/felt/dataprovider.conf \
 		        #etc/felt/validtime.conf \
 			#etc/felt/valueparameter.conf \
 			#etc/felt/levelparameter.conf \
 			#etc/felt/leveladditions.conf \
-                        #etc/felteps/felt2nc_vars_eps.xml \
-                        #etc/felteps/felt_axes.xml \
-                        #etc/felteps/felt_global_attributes.xml \
-                        #etc/felteps/felt_variables_eps.xml \
-                        #etc/felteps/dataprovider.conf \
-		        #etc/felteps/validtime.conf \
-			#etc/felteps/valueparameter.conf \
-			#etc/felteps/levelparameter.conf \
-			#etc/felteps/leveladditions.conf \
                         #etc/grib/fimexreader.xml \
                         #etc/grib/dataprovider.conf \
 			#etc/grib/valueparameter1.conf \
