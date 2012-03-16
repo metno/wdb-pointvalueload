ae73621d71e21659a257517dc9691f988717ae2d
Aleksandar Babic <aleksandar.babic@met.no>
2012-03-16 10:09:53 +0100

    initial documentation

---
+++ doc/doc.mk
@@ -0,0 +1,26 @@
+.xml.html:
+	$(DOCBOOK) html-nochunks $< -o `dirname $@`
+
+DOCUMENTATION_SRC = \
+	doc/developer/pointload_system-design-specification.xml \
+	doc/developer/pointload_system-test-specification.xml
+
+DOCUMENTATION_OUTPUT = $(DOCUMENTATION_SRC:.xml=.html)
+	
+html_DATA = $(DOCUMENTATION_OUTPUT)
+
+documentation:	$(DOCUMENTATION_OUTPUT)
+
+UNUSED_DOC = \
+	doc/user/pointLoad.man.xml
+
+USED_DOCS = \
+	doc/docbook/wdb_entities.ent \
+	doc/docbook/xinclude.mod
+	
+EXTRA_DIST += $(DOCUMENTATION_SRC) $(USED_DOCS)
+
+CLEANFILES += $(DOCUMENTATION_OUTPUT)
+
+include doc/doxygen/doxygen.mk
+include doc/man/man.mk
---
+++ doc/man/man.mk
@@ -0,0 +1,12 @@
+.xml.1:
+	$(DOCBOOK) man $< -o `dirname $@`
+
+.xml.5:
+	$(DOCBOOK) man $< -o `dirname $@`
+
+MAN_DOCS = doc/man/pointLoad.xml 
+
+man1_MANS = $(MAN_DOCS:.xml=.1)
+
+EXTRA_DIST += $(MAN_DOCS)
+CLEANFILES += $(MAN_DOCS:.xml=.1)
