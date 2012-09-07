6020bbe1c81fd511df72c52f689e26a56e9f7275
Aleksandar Babic <aleksandar.babic@met.no>
2012-09-07 12:53:00 +0200

    fixing packaging

--- etc/etc.mk
+++ etc/etc.mk
@@ -1,34 +1,45 @@
 confdir        = $(datadir)/pointLoad
-dist_conf_DATA = \
-					   etc/common/units.conf \
-					   etc/felt/dataprovider.conf \
-					   etc/felt/fimexreader.xml \
-					   etc/felt/leveladditions.conf \
-					   etc/felt/levelparameter.conf \
-					   etc/felt/load.conf \
-					   etc/felt/validtime.conf \
-					   etc/felt/valueparameter.conf \
-					   etc/felt/stations.nc \
-					   etc/felt/data.dat \
-					   etc/grib1/data.grib \
-					   etc/grib1/dataprovider.conf \
-					   etc/grib1/gribreader.xml \
-					   etc/grib1/leveladditions1.conf \
-					   etc/grib1/levelparameter1.conf \
-					   etc/grib1/load.conf \
-					   etc/grib1/stations.nc \
-					   etc/grib1/valueparameter1.conf \
-					   etc/grib2/data.grib \
-					   etc/grib2/dataprovider.conf \
-					   etc/grib2/gribreader.xml \
-					   etc/grib2/leveladditions2.conf \
-					   etc/grib2/levelparameter2.conf \
-					   etc/grib2/load.conf \
-					   etc/grib2/stations.nc \
-					   etc/grib2/valueparameter2.conf \
-					   etc/netcdf/data.nc \
-					   etc/netcdf/leveladditions.conf \
-					   etc/netcdf/levelparameter.conf \
-					   etc/netcdf/load.conf \
-					   etc/netcdf/stations.nc \
-					   etc/netcdf/valueparameter.conf
+commondir = $(confdir)/common
+dist_common_DATA = \
+                   etc/common/units.conf
+feltdir = $(confdir)/felt
+dist_felt_DATA = \
+                                           etc/felt/dataprovider.conf \
+                                           etc/felt/fimexreader.xml \
+                                           etc/felt/leveladditions.conf \
+                                           etc/felt/levelparameter.conf \
+                                           etc/felt/load.conf \
+                                           etc/felt/validtime.conf \
+                                           etc/felt/valueparameter.conf \
+                                           etc/felt/stations.nc \
+                                           etc/felt/data.dat
+
+grib1dir = $(confdir)/grib1
+dist_grib1_DATA = \
+                                           etc/grib1/data.grib \
+                                           etc/grib1/dataprovider.conf \
+                                           etc/grib1/gribreader.xml \
+                                           etc/grib1/leveladditions1.conf \
+                                           etc/grib1/levelparameter1.conf \
+                                           etc/grib1/load.conf \
+                                           etc/grib1/stations.nc \
+                                           etc/grib1/valueparameter1.conf
+grib2dir = $(confdir)/grib2
+dist_grib2_DATA = \
+                                           etc/grib2/data.grib \
+                                           etc/grib2/dataprovider.conf \
+                                           etc/grib2/gribreader.xml \
+                                           etc/grib2/leveladditions2.conf \
+                                           etc/grib2/levelparameter2.conf \
+                                           etc/grib2/load.conf \
+                                           etc/grib2/stations.nc \
+                                           etc/grib2/valueparameter2.conf
+
+netcdfdir = $(confdir)/netcdf
+dist_netcdf_DATA = \
+                                           etc/netcdf/data.nc \
+                                           etc/netcdf/leveladditions.conf \
+                                           etc/netcdf/levelparameter.conf \
+                                           etc/netcdf/load.conf \
+                                           etc/netcdf/stations.nc \
+                                           etc/netcdf/valueparameter.conf
